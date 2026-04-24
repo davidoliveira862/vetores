e9c339bc9f3901160b59cf67efda815ebadf48dc
davidoliveira862 <soaresdavid862@gmail.com>
2026-04-24 13:24:09 -0300

    vetores

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x;
+            int y;
+            double a;
+
+            string[] vet = Console.ReadLine()!.Split(' ');
+
+            x = vet[0];
+            y = int.Parse(vet[1]);
+            a = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);  
+            Console.WriteLine(a.ToString("F2", CultureInfo.InvariantCulture)); 
+
+            Console.ReadLine();
+
+        }
+    }
+}
